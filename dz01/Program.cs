db073b0750f17be057b86ac55c9ce06b293a328f
Pavel Starodubov <pablostar68@yahoo.com>
2022-10-21 23:05:05 +0300

    add dz01

--- dz01/Program.cs
+++ dz01/Program.cs
@@ -1,13 +1,14 @@
 ﻿// Найти точку пересечения двух прямых заданных уравнением 
-// y = k1 * x + b1, y = k2 * x + b2, b1 k1 и b2 и k2 заданы 
+// y = k1 * x + b1, y = k2 * x + b2, 
+// b1 k1 и b2 и k2 заданы 
 
 
-double k1=1;
-double b1=7;
-double k2=4;
-double b2=6;
+double k1 = 1;
+double b1 = 7;
+double k2 = 4;
+double b2 = 6;
 
-double x=(b2-b1)/(k1-k2);
-double y=k2*x+b2;
+double x = (b2 - b1) / (k1 - k2);
+double y = k2 * x + b2;
 
 Console.WriteLine($"Вывод координат пересечения: x = {x}    y = {y}");
