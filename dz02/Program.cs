bcb5342a685ef5fb647472f322e8ae8887307a3e
Pavel Starodubov <pablostar68@yahoo.com>
2022-10-22 10:00:54 +0300

    add dz02

--- dz02/Program.cs
+++ dz02/Program.cs
@@ -1,21 +1,21 @@
 ﻿//  Написать программу масштабирования фигуры
 
 
-Console.WriteLine("Введите первой точки: ");
+Console.WriteLine("Введите координаты первой точки: ");
 double x1= double.Parse(Console.ReadLine()?? "0");
 double y1= double.Parse(Console.ReadLine()?? "0");
-Console.WriteLine("Введите второй точки: ");
+Console.WriteLine("Введите координаты второй точки: ");
 double x2= double.Parse(Console.ReadLine()?? "0");
 double y2= double.Parse(Console.ReadLine()?? "0");
-Console.WriteLine("Введите третьей точки: ");
+Console.WriteLine("Введите координаты третьей точки: ");
 double x3= double.Parse(Console.ReadLine()?? "0");
 double y3= double.Parse(Console.ReadLine()?? "0");
-Console.WriteLine("Введите четвертой точки: ");
+Console.WriteLine("Введите координаты четвертой точки: ");
 double x4= double.Parse(Console.ReadLine()?? "0");
 double y4= double.Parse(Console.ReadLine()?? "0");
 
 Console.WriteLine("Задайте коэффициент масштабирования: ");
 double k= double.Parse(Console.ReadLine()?? "0");
 
-Console.WriteLine("Вывод получившихся коорлдинат: ");
+Console.WriteLine("Вывод получившихся координат: ");
 Console.WriteLine($"({x1*k},{y1*k})({x2*k},{y2*k})({x3*k},{y3*k})({x4*k},{y4*k})");
